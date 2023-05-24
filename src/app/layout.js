ecec65a05a9e802c43e77ecad4bfd69956bf877f
EastZero123 <cdy1005p@naver.com>
2023-05-24 23:17:03 +0900

    tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
-import "./globals.css"
-import { Inter } from "next/font/google"
-
-const inter = Inter({ subsets: ["latin"] })
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "IMDb Clobe",
   description: "This is the IMBd clobe website",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
